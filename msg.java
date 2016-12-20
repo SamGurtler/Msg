f637c0061f3faca39488f4900bec625c56744a08
Samuel C. Gurtler <StudentcareerSamgurtler@gmail.com>
2016-12-20 10:40:20 -0700

    Add files via upload

--- msg.java
+++ msg.java
@@ -12,45 +12,53 @@ public class msg {
     public static void main(String[] args) throws UnknownHostException, IOException {
 		ServerSocket Server = null;
 		Socket socket = null; 
-		String input,output = "Welcome!";
-		switch(args[0].toLowerCase()){
+		String input = "Welcome!";
+		String output = "Welcome!";
+		switch(getInput("Type \"create\" to create a conversation or \"join\" to join one:").toLowerCase()){
 			case "create":
-				Server = new ServerSocket(Integer.parseInt(args[1]),5);
+				Server = new ServerSocket(Integer.parseInt(getInput("Port:")),5);
 			 	System.out.print("Binding...");
 			 	socket = Server.accept();
 			 	System.out.println("Server has connected!");
 			 	BufferedReader isc = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			 	PrintWriter osc = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-			 	while(output != null||!output.toLowerCase().contains("/exit")){
+			 	while(true){
 				 	if((input=isc.readLine())!= null){
 						System.out.println(input);
 				   	}
+					if(input.toLowerCase().contains("/exit"))break;
 					output = getInput2();
 					osc.println(output);
 					osc.flush();
+					if(output.toLowerCase().contains("/exit"))break;
 				}
 				isc.close();
 				osc.close();
+				break;
 			case "join":
-				socket = new Socket(InetAddress.getByName(args[2]),Integer.parseInt(args[1]));
+				socket = new Socket(InetAddress.getByName(getInput("Host IP:")),Integer.parseInt(getInput("Port:")));
 				System.out.println("Client has connected to Server!");
 				BufferedReader is = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 				PrintWriter os = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-				while(output != null||!output.toLowerCase().contains("/exit")){
+				while(true){
+					if(input.toLowerCase().contains("/exit"))break;
 					output = getInput2();
 					os.println(output);
 					os.flush();
+					if(output.toLowerCase().contains("/exit"))break;
 					if((input=is.readLine())!= null) {
 						System.out.println(input);
 					}
 				}
 				is.close();
 				os.close();
+				break;
 		}
 	}
-	private static String getInput(){
+	private static String getInput(String print){
         	Scanner scan = new Scanner(System.in);
-		return scan.next();
+			System.out.print(print);
+			return scan.next();
 	} 
 	private static String getInput2()throws IOException{
 		BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
